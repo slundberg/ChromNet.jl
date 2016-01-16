80136fa8b6e639fbb39851896cc71e7fd7fdad92
Scott Lundberg <scottmlundberg@gmail.com>
2016-01-15 17:46:41 -0800

    Another update to help clean installs work

--- scripts/build_network_runner.jl
+++ scripts/build_network_runner.jl
@@ -5,6 +5,11 @@ try
 catch
     Pkg.clone("https://github.com/slundberg/ChromNet.jl.git")
 end
+try
+    @assert Pkg.installed("SamIO") != nothing
+catch
+    Pkg.clone("https://github.com/slundberg/SamIO.jl.git")
+end
 
 # This is just a simple link to the real script that is installed in the julia package.
 # By keeping this file as just a link, we can maintain the script using the Julia
